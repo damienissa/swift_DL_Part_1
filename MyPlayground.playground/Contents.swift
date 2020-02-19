d759faa2e6fe2e669717e355f34e7923a5fb2478
Dmitriy Lazarev <dimiklazarev@gmail.com>
2020-02-19 21:03:40 +0200

    Написал функцию которая переобразовывает Int в двоичный код

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
-написать функции поиска сумы массива
+
+// Написать функции поиска сумы массива
 
 func arraySum(array: [Int]) -> Int {
     var sum = 0
@@ -22,3 +23,17 @@ func arraySum(array: [Int]) -> Int {
 // Новое задание
 // Написать функицю которая будет преобразовывать Int в двоичную форму и посчитает сколько 1 в нем!
 // Приимер число 1234 в двоичном коде 0100 1101 0010 Ответ: 5
+
+// Функция переобразовывает Int в String в двоичном коде
+
+func convert(number: Int) -> String {
+
+
+return String(number, radix: 2)
+}
+
+let converted = convert(number: 1234)
+
+print(converted)
+
+
