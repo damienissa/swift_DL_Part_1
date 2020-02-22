b3af9adac03d3cf17fff0e75d7740661e3458aa9
Dmitriy Lazarev <dimiklazarev@gmail.com>
2020-02-22 13:13:21 +0200

    Отличие структуры от класса, наследование, перечисление, выучены.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -54,3 +54,125 @@ import UIKit
 // MARK: - Part 2 Classes and Structures
 
 /// ** Написать класс и структуру UserClass, UserStruct с полями имя, фамилия, возраст, место проживания **
+
+
+/*
+class UserClass {
+    
+    var name: String
+    var lastName: String
+    var age: Int
+    var address: String
+    
+    
+    init(name: String, lastName: String, age: Int, address: String) {
+        
+        self.name = name
+        self.lastName = lastName
+        self.age = age
+        self.address = address
+    }
+}
+
+
+let user1Class = UserClass(name: "Dima", lastName: "Lazarev", age: 21, address: "Kooperativna 9")
+
+print(user1Class.name, user1Class.lastName, user1Class.age, user1Class.address)
+
+
+struct UserStruct {
+    
+    var name: String
+    var lastName: String
+    var age: Int
+    var address: String
+}
+
+
+var usStruct = UserStruct(name: "Dima", lastName: "Lazarev", age: 21, address: "Kooperativna 9" )
+
+print(usStruct.name, usStruct.lastName, usStruct.age, usStruct.address)
+//при копировании класса мы копируем ссылку на объект
+let user2Class = user1Class
+//при копировании структуры мы копируем объект
+var usStruct2 = usStruct
+
+user2Class.address = "ZMIEVSKAYA 8"
+usStruct2.address = "ZMIEVSKAYA 8"
+
+print(user2Class.name, user2Class.lastName, user2Class.age, user2Class.address)
+print(usStruct2.name, usStruct2.lastName, usStruct2.age, usStruct2.address)
+
+print(user1Class.name, user1Class.lastName, user1Class.age, user1Class.address)
+print(usStruct.name, usStruct.lastName, usStruct.age, usStruct.address)
+ 
+ */
+/*
+enum PlayerState {
+    case none
+    case playing
+    case onPause
+    case forvarding
+    case backvarding
+    
+}
+
+class Player {
+    
+    var state: PlayerState = .none
+    
+    func startPlay() {
+        state = .playing
+    }
+}
+
+let player = Player()
+
+player.startPlay()
+print(player.state)
+*/
+
+enum FuelType {
+    
+    case bensine
+    case disel
+    case lpg
+}
+
+class Auto {
+    
+    var color: UIColor
+    var numberOfDors: Int
+    var fuelType: [FuelType]
+    
+    
+    init(color: UIColor, fuelType: [FuelType], numberOfDors: Int) {
+        
+        self.color = color
+        self.fuelType = fuelType
+        self.numberOfDors = numberOfDors
+    }
+}
+
+enum GruzType {
+    
+    case none
+    case samosval
+}
+
+class GruzoveAvto: Auto {
+    
+    var gruzopodyomnost: Int
+    var typeGruzovogo: GruzType
+    
+    init(color: UIColor, fuelType: [FuelType], numberOfDors: Int, gruzopodyomnost: Int, typeGruzovogo: GruzType) {
+        
+        self.gruzopodyomnost = gruzopodyomnost
+        self.typeGruzovogo = typeGruzovogo
+        super.init(color: color, fuelType: fuelType, numberOfDors: numberOfDors)
+    }
+}
+
+let kamaz = GruzoveAvto(color: .red , fuelType: [.bensine], numberOfDors: 2, gruzopodyomnost: 40, typeGruzovogo: .samosval)
+let toplivo: [FuelType] = [.bensine, .lpg]
+let romanovaSemerka = Auto(color: .white, fuelType: toplivo, numberOfDors: 4)
